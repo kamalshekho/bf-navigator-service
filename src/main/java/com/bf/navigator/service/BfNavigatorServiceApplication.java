2713e827f5c1a3195ad6a6e1368870b2cb92b108
Kamal Shekho <k.shekho@yahoo.com>
2026-03-23 13:34:14 +0100

    Add initial Spring Boot project structure

---
+++ src/main/java/com/bf/navigator/service/BfNavigatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bf.navigator.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfNavigatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfNavigatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bf/navigator/service/BfNavigatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bf.navigator.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfNavigatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
