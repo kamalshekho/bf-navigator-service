1f1cb413787de65732057012126ddc688c9d426f
Kamal Shekho <k.shekho@yahoo.com>
2026-03-24 20:24:23 +0100

    configure application properties and add security configuration

--- src/main/java/com/bf/navigator/service/common/WebClientConfig.java
+++ src/main/java/com/bf/navigator/service/common/WebClientConfig.java
@@ -2,23 +2,13 @@ package com.bf.navigator.service.common;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
-    }
-
-    @Bean
-    public WebClient webClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("https://apis.deutschebahn.com/db-api-marketplace/apis/station-data/v2")
-                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-                .build();
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
