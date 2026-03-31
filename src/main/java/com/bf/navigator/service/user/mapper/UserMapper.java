5abc360fdd2b8b15c97ebda352cc17cd0ddc541c
Kamal Shekho <k.shekho@yahoo.com>
2026-03-31 23:07:16 +0200

    feat: implement UserDTO, User, and AccessibilityType models with mapping adjustments

---
+++ src/main/java/com/bf/navigator/service/user/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.bf.navigator.service.user.mapper;
+
+import com.bf.navigator.service.user.dto.RegisterRequest;
+import com.bf.navigator.service.user.dto.UserDTO;
+import com.bf.navigator.service.user.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+/*
+ * UserMapper removed to avoid MapStruct lombok binding issues - manual mapping
+ * used
+ */
